e8c1035cdc8dd95b8dcc8ece40266fc50e4e8d2a
Hui Peng Hu <woohp135@gmail.com>
2023-01-15 02:09:44 +0000

    format using isort and black

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,57 +1,59 @@
 import unittest
+
 import numpy as np
+
 from value_encoder import ValueEncoder
 
 
 class ValueEncoderTestCases(unittest.TestCase):
     def test_basic(self):
-        encoder = ValueEncoder().fit('abced')
-        self.assertEqual(encoder.classes_, 'abcde')
+        encoder = ValueEncoder().fit("abced")
+        self.assertEqual(encoder.classes_, "abcde")
 
-        encoded = encoder.transform('aabec')
+        encoded = encoder.transform("aabec")
         self.assertEqual(encoded.dtype, np.int16)
         np.testing.assert_array_equal(encoded, [0, 0, 1, 4, 2])
 
         decoded = encoder.inverse_transform(encoded)
-        self.assertEqual(decoded, 'aabec')
+        self.assertEqual(decoded, "aabec")
 
     def test_batch(self):
-        encoder = ValueEncoder().fit('abced')
-        strings = ['aabec', 'abcd', 'ad']
+        encoder = ValueEncoder().fit("abced")
+        strings = ["aabec", "abcd", "ad"]
         encoded = encoder.transform(strings)
         np.testing.assert_array_equal(encoded, [[0, 0, 1, 4, 2], [0, 1, 2, 3, -1], [0, 3, -1, -1, -1]])
 
     def test_cap(self):
-        encoder = ValueEncoder().fit('abced')
-        encoded = encoder.transform('aabec', cap=True)
+        encoder = ValueEncoder().fit("abced")
+        encoded = encoder.transform("aabec", cap=True)
         np.testing.assert_array_equal(encoded, [0, 0, 1, 4, 2, 5])
 
     def test_batch_cap(self):
-        encoder = ValueEncoder().fit('abced')
-        strings = ['aabec', 'abcd', 'ad']
+        encoder = ValueEncoder().fit("abced")
+        strings = ["aabec", "abcd", "ad"]
         encoded = encoder.transform(strings, cap=True)
         np.testing.assert_array_equal(encoded, [[0, 0, 1, 4, 2, 5], [0, 1, 2, 3, 5, -1], [0, 3, 5, -1, -1, -1]])
 
     def test_batch_different_missing_value(self):
-        encoder = ValueEncoder().fit('abced')
-        strings = ['aabec', 'abcd', 'ad']
+        encoder = ValueEncoder().fit("abced")
+        strings = ["aabec", "abcd", "ad"]
         encoded = encoder.transform(strings, missing_value=100)
         np.testing.assert_array_equal(encoded, [[0, 0, 1, 4, 2], [0, 1, 2, 3, 100], [0, 3, 100, 100, 100]])
 
     def test_error(self):
         encoder = ValueEncoder()
-        encoder.fit('abcde')
+        encoder.fit("abcde")
         with self.assertRaisesRegex(ValueError, 'invalid character: "abcf"'):
-            encoder.transform('abcf')
+            encoder.transform("abcf")
         with self.assertRaisesRegex(ValueError, 'invalid character: "abc\t"'):
-            encoder.transform('abc\t')
+            encoder.transform("abc\t")
 
-        with self.assertRaisesRegex(ValueError, 'invalid character: 5'):
-            encoder.inverse_transform([0, 0, 5])
+        with self.assertRaisesRegex(ValueError, "invalid character: 5"):
+            encoder.inverse_transform(np.array([0, 0, 5]))
 
     def test_fit_duplicates(self):
         encoder = ValueEncoder()
-        encoder.fit('abceda')
-        self.assertEqual(encoder.classes_, 'abcde')
-        encoded = encoder.transform('aabec')
+        encoder.fit("abceda")
+        self.assertEqual(encoder.classes_, "abcde")
+        encoded = encoder.transform("aabec")
         np.testing.assert_array_equal(encoded, [0, 0, 1, 4, 2])
